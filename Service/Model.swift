c3162e90b72d0d9844caa18864e14898f707328c
Paul <blackjug87@gmail.com>
2023-03-22 15:33:04 +0700

    Model prepare

---
+++ Service/Model.swift
@@ -0,0 +1,36 @@
+//
+//  Model.swift
+//  TestTaskMVVM
+//
+//  Created by Paul on 22.03.2023.
+//
+
+import Foundation
+
+struct MainData: Codable {
+    let id: Int
+    let startAddress: Address
+    let endAddress: Address
+    let Price: Price
+    let orderTime: Date
+    let car: Car
+}
+
+struct Address: Codable {
+    let city: String
+    let address: String
+}
+
+struct Price: Codable {
+    let amount: Int
+    let currency: String
+}
+
+struct Car: Codable {
+    let autoNumber: Int
+    let autoModel: String
+    let autoPhoto: String
+    let driver: String
+}
+
+typealias FullData = [MainData]
